6528d6446dbb637bfd0e051c741829c2610cd218
David Moreno <dmoreno@coralbits.com>
2019-05-31 18:28:16 +0200

    Use test framework on mDNS tests

--- tests/test_mdns.cpp
+++ tests/test_mdns.cpp
@@ -17,17 +17,30 @@
  */
 #include <cstdint>
 #include "../src/netutils.hpp"
+#include "../src/logger.hpp"
 #include "./testcase.hpp"
+#include "./test_utils.hpp"
 
-uint8_t packet_res[] = {
-  0x00, 0x00, 0x84, 0x00 , 0x00, 0x00, 0x00, 0x01 , 0x00, 0x00, 0x00, 0x00 , 0x0B, 0x5F, 0x61, 0x70, // 16
-  0x70, 0x6C, 0x65, 0x2D , 0x6D, 0x69, 0x64, 0x69 , 0x04, 0x5F, 0x75, 0x64 , 0x70, 0x05, 0x6C, 0x6F, // 32
-  0x63, 0x61, 0x6C, 0x00 , 0x00, 0x0C, 0x00, 0x01 , 0x00, 0x00, 0x00, 0x78 , 0x00, 0x0D, 0x0A, 0x75, // 48
-  0x63, 0x75, 0x62, 0x65 , 0x2D, 0x77, 0x69, 0x6E , 0x65, 0xC0, 0x0B
-};
+auto packet_dns = hex_to_bin(
+  "00 00 84 00" // DNS header and flags
+  "00 00 00 01" // 1 question
+  "00 00 00 01" // 1 answer
+  // question, pos 12
+  "0B '_apple-midi' 04 '_udp' 05 'local' 00"
+  "00 0C" // PTR
+  "00 01" // Class IN
+  // answer, pos 40
+  "C0 0C" // label pointer to question label
+  "00 0C" // PTR
+  "00 01" // Class IN
+  "00 00 10 00" // TTL
+  "00 0C" // data length, 13 bytes
+  "0B 'test-server' C0 0C" // pos 52
+);
 
-bool test_label_simple(){
-  auto buffer = rtpmidid::parse_buffer_t(packet_res, sizeof(packet_res));
+void test_label_simple(){
+  auto buffer = *packet_dns;
+  buffer.print_hex(true);
   uint8_t label[120];
   auto label_buffer = rtpmidid::parse_buffer_t(label, sizeof(label));
 
@@ -35,27 +48,25 @@ bool test_label_simple(){
   buffer.position += 12;
   rtpmidid::read_label(buffer, label_buffer);
 
-  if (strncmp((char*)label, "_apple-midi._udp.local", sizeof(label)) != 0){
-    fprintf(stderr, "Label does not match: %s\n", label);
-    return false;
-  }
-  return true;
+  ASSERT_EQUAL(strncmp((char*)label, "_apple-midi._udp.local", sizeof(label)), 0);
 }
 
-bool test_label_w_pointer(){
-  auto buffer = rtpmidid::parse_buffer_t(packet_res, sizeof(packet_res));
+void test_label_w_pointer(){
+  auto buffer = *packet_dns;
   uint8_t label[120];
   auto label_buffer = rtpmidid::parse_buffer_t(label, sizeof(label));
 
   // label with pointer position
-  buffer.position += 46;
+  buffer.position = buffer.start + 40;
   rtpmidid::read_label(buffer, label_buffer);
+  DEBUG("{}", (char*)&label);
+  ASSERT_EQUAL(strncmp((char*)label, "_apple-midi._udp.local", sizeof(label)), 0);
 
-  if (strncmp((char*)label, "_apple-midi._udp.local", sizeof(label)) != 0){
-    fprintf(stderr, "Label with pointer does not match: %s\n", label);
-    return false;
-  }
-  return true;
+  buffer.position = buffer.start + 52;
+  label_buffer.position = label_buffer.start;
+  rtpmidid::read_label(buffer, label_buffer);
+  DEBUG("{}", (char*)&label);
+  ASSERT_EQUAL(strncmp((char*)label, "test-server._apple-midi._udp.local", sizeof(label)), 0);
 }
 
 
