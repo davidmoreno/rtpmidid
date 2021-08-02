f6dfc2d6b5664eaa5d7100b25934c84a25e22051
folkert van heusden <mail@vanheusden.com>
2021-08-02 23:37:22 +0200

    Debug-output fix

--- lib/rtpclient.cpp
+++ lib/rtpclient.cpp
@@ -179,13 +179,13 @@ void rtpclient::connect_to(const std::string &address,
     freeaddrinfo(sockaddress_list);
   }
 
-  DEBUG("Connecting control port {} to {}:{}", local_base_port, host, service);
+  DEBUG("Connecting midi port {} to {}:{}", local_base_port + 1, address, remote_base_port + 1);
 
   // If not connected, connect now the MIDI port
   auto conn_event = peer.connected_event.connect(
       [this, address, port](const std::string &name, rtppeer::status_e status) {
         if (status == rtppeer::CONTROL_CONNECTED) {
-          DEBUG("Connecting midi port {} to {}:{}", local_base_port + 1,
+          DEBUG("Connected midi port {} to {}:{}", local_base_port + 1,
                 address, remote_base_port + 1);
           peer.connect_to(rtppeer::MIDI_PORT);
         } else if (status == rtppeer::CONNECTED) {
