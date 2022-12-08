db5b30f0806899d902a9d45f4a0d85cc5eb0dac9
David Moreno <dmoreno@coralbits.com>
2022-12-08 22:05:52 +0100

    Very basic event parser with some examples
    No real protocol yet, but has all the required
    elements to now build upon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
+mod rtppeer;
+
+/**
+ * Real Time Protocol Music Instrument Digital Interface Daemon
+ * Copyright (C) 2019-2023 David Moreno Montero <dmoreno@coralbits.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+use std::io;
+use std::str;
+use tokio::net::UdpSocket;
+
+use crate::rtppeer::RtpPeerEvent;
+use crate::rtppeer::RtpPeerEventResponse;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
     println!("Hello, world!");
+
+    let socket = UdpSocket::bind("0.0.0.0:6789").await?;
+
+    let mut rtppeer = rtppeer::RtpPeer::new();
+    let mut buf = vec![0u8; 1500];
+    loop {
+        let (length, addr) = socket.recv_from(&mut buf).await?;
+        let event = RtpPeerEvent::MidiData(&buf[0..length as usize]);
+        match rtppeer.event(&event) {
+            RtpPeerEventResponse::MidiData(data) => {
+                socket.send_to(data, addr).await?;
+            }
+            _ => {
+                println!("WIP");
+            }
+        }
+    }
+
+    // Ok(())
 }
---
+++ src/rtppeer.rs
@@ -0,0 +1,122 @@
+use std::{
+    io::{self, Cursor, Write},
+    time::Duration,
+};
+
+#[derive(Debug, PartialEq)]
+enum RtpPeerStatus {
+    Initial,
+    ControlConnected,
+    MidiConnected,
+    Connected,
+    WaitingCk,
+    Disconnected,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum RtpPeerEventBasic {
+    SendCk,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum RtpPeerEvent<'a> {
+    ControlData(&'a [u8]),
+    MidiData(&'a [u8]),
+    SendCk,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum RtpPeerEventResponse<'a> {
+    DoNothing,
+    MidiData(&'a [u8]),
+    ControlData(&'a [u8]),
+    ScheduleTimeout(Duration, RtpPeerEventBasic),
+    Disconnect,
+}
+
+#[derive(Debug)]
+pub(crate) struct RtpPeer {
+    status: RtpPeerStatus,
+    initiator_id: u16,
+    sequence_nr: u32,
+    sequence_ack: u32,
+    remote_sequence_nr: u32,
+    timestamp_start: u64,
+    latency: u64,
+    // Part of the struct, to prevent mallocs at return.
+    // No mem management needed for this type.
+    buffer: [u8; 1500],
+}
+
+impl RtpPeer {
+    pub fn new() -> RtpPeer {
+        RtpPeer {
+            status: RtpPeerStatus::Initial,
+            initiator_id: 0,
+            sequence_nr: 0,
+            sequence_ack: 0,
+            remote_sequence_nr: 0,
+            timestamp_start: 0,
+            latency: 0,
+            buffer: [0; 1500],
+        }
+    }
+
+    pub fn event(&mut self, event: &RtpPeerEvent) -> RtpPeerEventResponse {
+        println!("Got event: {:?}", event);
+        match event {
+            RtpPeerEvent::MidiData(data) => {
+                return self.parse_packet(data);
+            }
+            _ => {
+                println!("Rest")
+            }
+        }
+        RtpPeerEventResponse::DoNothing
+    }
+
+    fn parse_packet(&mut self, data: &[u8]) -> RtpPeerEventResponse {
+        println!("GOT Data {:?}", data);
+
+        let mut cursor = Cursor::new(&mut self.buffer[..]);
+        // Write::write(&mut cursor, b"test");
+        cursor.write(&[0xFF, 0xFF]).unwrap();
+        cursor.write(b"IN").unwrap();
+        cursor.write(&[0xFA, 0x57]).unwrap();
+        cursor.write(&[0xBE, 0xEF]).unwrap();
+        cursor.write(b"response").unwrap();
+        cursor.write(&[0x00]).unwrap();
+
+        let length = cursor.position() as usize;
+        return RtpPeerEventResponse::MidiData(&self.buffer[..length]);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::rtppeer::{RtpPeerEventResponse, RtpPeerStatus};
+
+    use super::{RtpPeer, RtpPeerEvent};
+    #[test]
+    fn test_rtppeer_new() {
+        let mut rtppeer = RtpPeer::new();
+
+        assert!(rtppeer.status == RtpPeerStatus::Initial);
+        let ret = rtppeer.event(&RtpPeerEvent::MidiData(&[
+            // rtpmidi connect message
+            0xFF, 0xFF, b'I', b'N', // command in
+            0xFA, 0x57, 0xBE, 0xEF, // Version, and Id
+            b't', b'e', b's', b't', b'i', b'n', b'g', 0x00, // The name
+        ]));
+        println!("{:?}", ret);
+        let sdata = match ret {
+            RtpPeerEventResponse::MidiData(sdata) => sdata,
+            _ => panic!("Bad type"),
+        };
+        assert_eq!(sdata.len(), 3);
+
+        let ret = rtppeer.event(&RtpPeerEvent::SendCk);
+
+        println!("{:?}", ret);
+    }
+}
