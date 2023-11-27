72248ee1d0c291e195f04fd805e47da45f16474e
David Moreno <dmoreno@coralbits.com>
2023-11-27 23:26:52 +0100

    Optional precompiled headers.
    Still to decide if it's worth.

---
+++ lib/precompile.hpp
@@ -0,0 +1,28 @@
+/**
+ * Real Time Protocol Music Instrument Digital Interface Daemon
+ * Copyright (C) 2019-2023 David Moreno Montero <dmoreno@coralbits.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#pragma once
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <iterator>
+#include <rtpmidid/exceptions.hpp>
+#include <rtpmidid/iobytes.hpp>
+#include <rtpmidid/logger.hpp>
+#include <rtpmidid/poller.hpp>
+#include <rtpmidid/signal.hpp>
