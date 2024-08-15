b810279b69d830f00c6bf44ff8a7c9f59ca62c1a
David Moreno <dmoreno@coralbits.com>
2024-08-15 23:50:00 +0200

    Add rtpserverpeer class. rtpserver uses it.
    It replaces the internal struct and weird management of the struct lifecycle inside the server. Now each serverpeer has an easier to amnage lifecycle.
    
    This also helped to split all connecctions into custom functions that helps a bit when debugging, but I hope will be fully optimized away at final compile time.

--- include/rtpmidid/rtpserver.hpp
+++ include/rtpmidid/rtpserver.hpp
@@ -23,6 +23,7 @@
 #include <memory>
 #include <rtpmidid/poller.hpp>
 #include <rtpmidid/rtppeer.hpp>
+#include <rtpmidid/rtpserverpeer.hpp>
 #include <rtpmidid/signal.hpp>
 #include <rtpmidid/udppeer.hpp>
 
@@ -35,22 +36,7 @@ public:
   signal_t<const io_bytes_reader &> midi_event;
 
   int max_peer_data_id = 1;
-  struct peer_data_t {
-    int id;
-    std::shared_ptr<rtppeer_t> peer;
-
-    rtppeer_t::send_event_t::connection_t send_event_connection;
-    rtppeer_t::status_change_event_t::connection_t
-        status_change_event_connection;
-
-    rtppeer_t::midi_event_t::connection_t midi_event_connection;
-    poller_t::timer_t timer_connection;
-
-    connection_t<float> timer_ck_connection;
-    poller_t::timer_t timer_ck;
-    network_address_t addr;
-  };
-  std::vector<peer_data_t> peers;
+  std::vector<rtpserverpeer_t> peers;
 
   std::string name;
   udppeer_t control;
@@ -64,25 +50,21 @@ public:
 
   int port() { return control.get_address().port(); }
 
-  // Returns the peer for that packet, or nullptr
-  std::shared_ptr<rtppeer_t> get_peer_by_packet(io_bytes_reader &b,
-                                                rtppeer_t::port_e port);
-  std::shared_ptr<rtppeer_t> get_peer_by_initiator_id(uint32_t initiator_id);
-  std::shared_ptr<rtppeer_t> get_peer_by_ssrc(uint32_t ssrc);
-
   void create_peer_from(io_bytes_reader &&buffer, const network_address_t &addr,
                         rtppeer_t::port_e port);
+  void remove_peer(int peer_id);
 
   void send_midi_to_all_peers(const io_bytes_reader &bufer);
-  // Call from time to time when there are events to
-  // avoid disconnection. Normally on cks.
-  void rearm_ck_timeout(int peerdata_id);
 
   void data_ready(const io_bytes_reader &data, const network_address_t &addr,
                   rtppeer_t::port_e port);
   void sendto(const io_bytes_reader &b, rtppeer_t::port_e port,
               network_address_t &address, int remote_base_port);
 
-  peer_data_t *find_peer_data_by_id(int id);
+  rtpserverpeer_t *find_peer_data_by_id(int id);
+  rtpserverpeer_t *find_peer_by_packet(io_bytes_reader &b,
+                                       rtppeer_t::port_e port);
+  rtpserverpeer_t *find_peer_by_initiator_id(uint32_t initiator_id);
+  rtpserverpeer_t *find_peer_by_ssrc(uint32_t ssrc);
 };
 } // namespace rtpmidid
